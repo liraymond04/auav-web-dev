4019d5de9bf786c5e0ac29c4ccef4de32a31c599
Raymond Li <li.raymond04@gmail.com>
2025-07-31 15:01:15 -0600

    Move code example templates to shared file

---
+++ src/lib/components/filetree/shared.ts
@@ -0,0 +1,85 @@
+export const usage_file_code = `
+  <script lang="ts">
+    let file_node: FileNode = {
+      type: 'file',
+      name: '+page.svelte',
+      extension: 'svelte'
+    }
+  <\/script>
+
+  <FileNodeComponent
+    node={file_node}
+    depth={0}
+    path={'routes/+page.svelte'}
+  />
+`
+
+export const def_file_code = `
+  <!-- FileNode.svelte -->
+  <script lang="ts">
+    const { node, depth, path } = $props()
+
+    function handleClick() {
+      console.log(path)
+    }
+  <\/script>
+
+  <div
+    onclick={handleClick}
+  >
+    <FileIcon extension={node.extension} />
+    <span>{node.name}</span>
+  </div>
+`
+
+export const usage_dir_code = `
+  <script lang="ts">
+    let nodes: FileTreeNode[] = $state([
+      {
+        type: 'directory',
+        name: 'routes',
+        isExpanded: false,
+        children: [file_node]
+      }
+    ])
+    let dir_node: DirectoryNode =
+      $derived(nodes[0] as DirectoryNode)
+  <\/script>
+
+  <DirectoryNodeComponent
+    bind:nodes
+    node={dir_node}
+    depth={0}
+    path={'routes'}
+  />
+`
+
+export const def_dir_code = `
+  <!-- DirectoryNode.svelte -->
+  <script lang="ts">
+    const { nodes = $bindable, node, depth, path } = $props()
+
+    function handleToggle() {
+      updateNodeByPath(nodes, path, (n) => {
+        n.isExpanded = !n.isExpanded
+      })
+    }
+  <\/script>
+
+  <div onclick={handleToggle} >
+    {#if node.isExpanded}
+      <Icon icon="mdi:chevron-down"></Icon>
+    {:else}
+      <Icon icon="mdi:chevron-right"></Icon>
+    {/if}
+
+    {#if node.isExpanded}
+      <Icon icon="mdi:folder-open-outline"></Icon>
+    {:else}
+      <Icon icon="mdi:folder-outline"></Icon>
+    {/if}
+
+    <span>{node.name}</span>
+    <span>{node.children.length} items</span>
+  </div>
+`
--- src/slides/120/slide.svelte
+++ src/slides/120/slide.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import DirectoryNodeComponent from '$lib/components/filetree/DirectoryNode.svelte'
 	import FileNodeComponent from '$lib/components/filetree/FileNode.svelte'
-	import { def_dir_code, def_file_code, usage_dir_code, usage_file_code } from '$lib/components/filetree/shared'
+	import { usage_dir_code, usage_file_code } from '$lib/components/filetree/shared'
 	import type { DirectoryNode, FileNode, FileTreeNode } from '$lib/components/filetree/types'
 	import { Action, Code, Transition } from '@animotion/core'
 
@@ -25,22 +25,15 @@
 
 	let file_code: ReturnType<typeof Code>
 	let dir_code: ReturnType<typeof Code>
-
-	let showPrev = $state(true)
 </script>
 
 <div class="relative flex min-h-screen flex-col">
 	<div class="flex flex-grow flex-col items-center justify-center">
 		<Transition visible>
-			<p bind:this={text} class="p-8 text-center text-5xl font-bold">Separation of Concerns</p>
+			<p bind:this={text} class="p-8 text-center text-5xl font-bold">Reusability via components</p>
 		</Transition>
 		<div class="flex gap-x-8">
-			{#if showPrev}
-				<Transition>
-					<iframe title="slide_content" class="min-h-80 min-w-120" src="/embed/filetree"></iframe>
-				</Transition>
-			{/if}
-			<Transition do={() => (showPrev = false)} undo={() => (showPrev = true)}>
+			<Transition>
 				<div class="pt-2 pb-2">
 					<FileNodeComponent node={file_node} depth={0} path={'routes/+page.svelte'} />
 				</div>
@@ -59,13 +52,7 @@
 							enhanceMatching: true
 						}}
 					/>
-					<Action
-						do={() => file_code.update`${def_file_code}`}
-						undo={() => file_code.update`${usage_file_code}`}
-					/>
 				</div>
-			</Transition>
-			<Transition>
 				<div class="pt-2 pb-2">
 					<DirectoryNodeComponent bind:nodes node={dir_node} depth={0} path={'routes'} />
 				</div>
@@ -83,10 +70,6 @@
 							containerStyle: false,
 							enhanceMatching: true
 						}}
-					/>
-					<Action
-						do={() => dir_code.update`${def_dir_code}`}
-						undo={() => dir_code.update`${usage_dir_code}`}
 					/>
 				</div>
 			</Transition>
