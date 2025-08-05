7a0656521bba4d40b1f0d3f2c61cb2e69fbdbeae
Raymond Li <li.raymond04@gmail.com>
2025-08-05 02:09:26 -0600

    Add iframe slide for todo list

---
+++ src/slides/420/slide.svelte
@@ -0,0 +1,5 @@
+<div class="relative flex min-h-screen flex-col">
+	<div class="flex flex-grow flex-col items-center justify-center">
+		<iframe title="slide_content" class="min-h-80 min-w-120" src="/embed/todo-list"></iframe>
+	</div>
+</div>
