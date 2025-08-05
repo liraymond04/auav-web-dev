7a32fc99933f0226a546294d542140ec260ee860
Raymond Li <li.raymond04@gmail.com>
2025-08-05 11:27:31 -0600

    Add more links and instructions for hands-on project slide

--- src/slides/500/slide.svelte
+++ src/slides/500/slide.svelte
@@ -1,6 +1,21 @@
 <div class="h-full min-h-screen place-content-center place-items-center">
-	<p class="text-5xl font-bold">Hands on Project</p>
+	<p class="text-5xl font-bold">Hands-on Project</p>
 	<ul class="mt-6 space-y-2 text-left text-2xl">
-		<li>https://github.com/liraymond04/sveltekit-template</li>
+		<li>
+			<div>Repo for this presentation (also built with Sveltekit),</div>
+			<div>
+				Includes example code in the <span class="p-1 bg-base-100 rounded font-mono">src/routes/embed</span>
+				directory
+			</div>
+			<a href="https://github.com/liraymond04/auav-web-dev">
+				https://github.com/liraymond04/auav-web-dev
+			</a>
+		</li>
+		<li>
+			<div>Template to start writing your project</div>
+			<a href="https://github.com/liraymond04/sveltekit-template">
+				https://github.com/liraymond04/sveltekit-template
+			</a>
+		</li>
 	</ul>
 </div>
